a729c1d9a8520ea29375c1b1f68c0a1a5e1a2f02
SamHsu <yxu166@jh.edu>
2026-09-04 10:04:29 -0400

    chore(release): publish 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
     group = "edu.jhu.cobra"
-    version = "0.1.2"
+    version = "0.2.0"
 
     repositories {
         mavenCentral()
